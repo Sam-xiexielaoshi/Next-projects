e27a86f9c46272faa508007bba0acecaf07c3ab3
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-06-30 21:01:24 +0530

    metadata

--- first-next/src/app/blog/page.tsx
+++ first-next/src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog|pokemon",
+  },
+};
+
 export default function Blog() {
   return <h1>My blog</h1>;
 }
