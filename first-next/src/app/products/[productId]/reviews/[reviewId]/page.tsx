831bff31e22a9d29814fc7424a74821bacd86ed7
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-07-02 14:33:49 +0530

    error handling

--- first-next/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ first-next/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,14 +1,20 @@
 import { redirect } from "next/navigation";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default async function ProductReview({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
+  const random = getRandomInt(2);
+  if (random === 1) throw new Error("Error in reviewId");
   const { productId, reviewId } = await params;
-  if (parseInt(reviewId) > 1000) 
+  if (parseInt(reviewId) > 1000)
     // notFound();
-  redirect('/products')
+    redirect("/products");
   return (
     <h1>
       Review {reviewId} for product {productId}
