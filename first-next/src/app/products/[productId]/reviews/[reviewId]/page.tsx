9acd139ea0929d89c6abf2a067e84acf2452a5e2
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-07-02 13:44:23 +0530

    navigation programmatically

--- first-next/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ first-next/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from "next/navigation";
+import { notFound,redirect } from "next/navigation";
 
 export default async function ProductReview({
   params,
@@ -6,7 +6,9 @@ export default async function ProductReview({
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
   const { productId, reviewId } = await params;
-  if (parseInt(reviewId) > 1000) notFound();
+  if (parseInt(reviewId) > 1000) 
+    // notFound();
+  redirect('/products')
   return (
     <h1>
       Review {reviewId} for product {productId}
